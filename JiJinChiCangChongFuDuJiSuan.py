7ad62ae88aa791197095e36ad5406ab723a7b631
liuboyan12 <594303711@qq.com>
2018-04-23 18:00:22 +0800

    20180423

--- JiJinChiCangChongFuDuJiSuan.py
+++ JiJinChiCangChongFuDuJiSuan.py
@@ -14,16 +14,29 @@ def stock_list(fundcode):
 
 
 if __name__ == '__main__':
+    """
+    code_list = ['002001','070001']
+    result_list = []
+    for i in code_list:        #002001
+        passcode = 0
+        while(passcode<1):
+            try:
+                _stock_list=stock_list(i)    #"['0008582', '0003332', '6011551', '0005682', '6038331', '6002761', '0020432', '0007862', '0009632', '6038161']"
+                #{'002001':[]}
+                _stock_list = "'"+i+"':"+str(_stock_list)+','
 
-    while(passcode<1):
-        try:
-            _stock_list=stock_list('002001')
-            passcode = 0
-        except Exception as e :
-            if e =='' :
-                passcode =1
-            pass
+                passcode = 1
+            except Exception as e :
+                pass
+        result_list.append(_stock_list)
+        """
+    result_list = eval("{'002001':['0003332', '0008582'],'002002':[ '0003332', '6011551']}")
+    print(type(result_list))
+
+    # print(result_list[0])
+
+
 
     # print(_stock_list)
-    # _stock_list =eval( "['0008582', '0003332', '6011551', '0005682', '6038331', '6002761', '0020432', '0007862', '0009632', '6038161']")
-    print(len(_stock_list))
+    #     stock_list =eval( "['0008582', '0003332', '6011551', '0005682', '6038331', '6002761', '0020432', '0007862', '0009632', '6038161']")
+    # print(len(result_list))
