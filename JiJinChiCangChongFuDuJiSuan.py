853d2a9aebdbb5d9785705929712673b6948edca
liuboyan12 <594303711@qq.com>
2018-04-23 17:38:04 +0800

    20180423

--- JiJinChiCangChongFuDuJiSuan.py
+++ JiJinChiCangChongFuDuJiSuan.py
@@ -14,6 +14,16 @@ def stock_list(fundcode):
 
 
 if __name__ == '__main__':
-    # _stock_list=stock_list('002001')
+
+    while(passcode<1):
+        try:
+            _stock_list=stock_list('002001')
+            passcode = 0
+        except Exception as e :
+            if e =='' :
+                passcode =1
+            pass
+
     # print(_stock_list)
-    _stock_list = "['0008582', '0003332', '6011551', '0005682', '6038331', '6002761', '0020432', '0007862', '0009632', '6038161']"
+    # _stock_list =eval( "['0008582', '0003332', '6011551', '0005682', '6038331', '6002761', '0020432', '0007862', '0009632', '6038161']")
+    print(len(_stock_list))
