b0285bb7cdd16a2f6aa474f6c496a0488888017e
liuboyan12 <594303711@qq.com>
2018-04-24 18:06:31 +0800

    20180424

--- JiJinChiCangChongFuDuJiSuan.py
+++ JiJinChiCangChongFuDuJiSuan.py
@@ -30,8 +30,29 @@ if __name__ == '__main__':
                 pass
         result_list.append(_stock_list)
         """
+    #<urlopen error [WinError 10060] 由于连接方在一段时间后没有正确答复或连接的主机没有反应，连接尝试失败。>
     result_list = eval("{'002001':['0003332', '0008582'],'002002':[ '0003332', '6011551']}")
-    print(type(result_list))
+    # print(type(result_list)
+    _dict_key = list(result_list.keys())
+    # print(type(_dict_key))
+    _result_dict = {}
+    for i in _dict_key:
+        ifcode  = _dict_key.index(i)
+        if ifcode == 0:
+            creat_list = result_list[_dict_key[1]]
+            for i in creat_list:
+                print(i)
+                _string = str(i)+':1'
+                print(_string)
+                _result_dict.update(_string)
+            print(_result_dict)
+
+            # print(_result_dict)
+        # print(_dict_key[i])
+    #     # print(ifcode)
+        # print(result_list[_dict_key[i]])
+
+
 
     # print(result_list[0])
 
