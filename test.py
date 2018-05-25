64bd4148401f1903abd5e16666ea528e4feb7025
liuboyan12 <594303711@qq.com>
2018-05-25 18:00:43 +0800

    DATE

---
+++ test.py
@@ -0,0 +1,10 @@
+
+def list(list1,list2):
+    for i in range(len(list1)):
+        num1=list1[i]
+        num2=list2[i]
+
+if __name__ == '__main__':
+    list1 = [1, 2, 3, 4]
+    list2 = [0, 3, 4, 5]
+    a = list(list1,list2)
