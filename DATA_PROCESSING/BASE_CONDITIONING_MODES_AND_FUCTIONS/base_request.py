3b66eb3e5a2610b49cbc5a64cfbffd0102655424
liuboyan <594303711@qq.com>
2018-07-30 14:14:54 +0800

    20180730-1

--- DATA_PROCESSING/BASE_CONDITIONING_MODES_AND_FUCTIONS/base_request.py
+++ DATA_PROCESSING/BASE_CONDITIONING_MODES_AND_FUCTIONS/base_request.py
@@ -2,7 +2,9 @@
 import urllib.request
 def base_request(str):
     response = urllib.request.urlopen(str)
-    return response.read().decode(encoding='utf-8')
+    returnvalue = response.read().decode(encoding='utf-8')
+    response.close()
+    return returnvalue
 
 if __name__ == '__main__':
     print()
